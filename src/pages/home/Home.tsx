db73023b379201b1f797250900989c96fce67125
hulys <hulyssesmaga@gmail.com>
2024-09-08 20:06:54 -0300

    iniciando estruturcao da landing page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import '../../styles/utility.css'
+import Header from '../header'
+import Hero from '../Hero'
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Hero />
+        </>
+    )
+}
