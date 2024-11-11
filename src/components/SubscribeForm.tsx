7383f5e7eedcfa0d5f2ecc6739c06314444d2e03
hulysses <hulyssesmaga@gmail.com>
2024-11-11 11:01:30 -0300

    corrigindo erro de rota do google cloud apos o deploy

--- src/components/SubscribeForm.tsx
+++ src/components/SubscribeForm.tsx
@@ -16,7 +16,10 @@ export default function SubscribeForm() {
 
     const sendEmail = async () => {
         try {
-            const response = await axios.post('/function-1', formData, {
+            const baseUrl = process.env.NODE_ENV === 'production'
+                ? 'https://us-central1-arch-lambda-htech.cloudfunctions.net/function-1'
+                : '/function-1';
+            const response = await axios.post(baseUrl, formData, {
                 headers: {
                     'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjFkYzBmMTcyZThkNmVmMzgyZDZkM2EyMzFmNmMxOTdkZDY4Y2U1ZWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA4Mzk3NjY0NzgwNTY0ODUzNTM0IiwiZW1haWwiOiJodWx5c3Nlc21hZ2FAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJFVVNaTi1uTkhIVVlMVENjTHJ0NzVnIiwibmJmIjoxNzMxMzMyNjcxLCJpYXQiOjE3MzEzMzI5NzEsImV4cCI6MTczMTMzNjU3MSwianRpIjoiMTAzZjM3YWQxN2U4ZDQ1ZjhiYjcwM2JmMTk1NGQ5YzFhNDQzMWE1YiJ9.Lvus6NdEPh1XuAY47D_BXSzI-qOkcY9lcQz5Q5jc5ejBjbaXpQuWfzhsvw8v5zkgHq441L9AbxEXQBJZpES7i6WwCTBTRUzuGE4oMmea6ZJDz4kUm5dadApvvgjOEaJXm9WmJLW2uhi9R1jcXWb7dXlNTEq4pxWCkDFPGXjL2I6GyAOXjtkVH-Nu0tB_1hGAE2JkIOXpjkGa-AQVxJDKItF_SsqU2jYR3mU6FFuCslYBNCd51pwTBV6HS4ouKDIhreddWo0kSFc6P2yMG9XWlMw1PSwFdnqWnLHYQMsUkS2WGd87sq3RI6s2tvFfpyvRCoURjiOGz_Q4bFJnppcMXg`
                 }
