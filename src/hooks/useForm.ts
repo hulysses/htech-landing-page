4f3c6c460693f2c8d9fe489a14a6e8eb6b88caee
hulysses <hulyssesmaga@gmail.com>
2024-11-18 21:27:20 -0300

    refactor useForm and emailService for improved security and clarity; remove hardcoded authorization token

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -6,6 +6,7 @@ import { useState } from "react";
  * Recebe como parametros initialValues - Valores iniciais do formulário.
  * Retorna Um objeto contendo os dados do formulário, a função de manuseio de mudanças e a função de reset.
  */
+
 export function useForm<T>(initialValues: T) {
   const [formData, setFormData] = useState<T>(initialValues);
 
